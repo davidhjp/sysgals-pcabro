67e0a1150775315a9a9cc50e97bbf9edd382eff6
Heejong Park <hpar081@aucklanduni.ac.nz>
2017-02-08 11:45:13 +1300

    removed the Buffer clock-domain in pcabro_q.sysj

--- fib/config_q.c
+++ fib/config_q.c
@@ -1,6 +1,5 @@
 #include "sysgals.h"
 
 void sysgals_initialize() {
-	sgl_ConnectQ(&__q_Pabro_C1,  &__q_Buffer_C1);
-	sgl_ConnectQ(&__q_Buffer_C2, &__q_Cabro_C2);
+	sgl_ConnectQ(&__q_Pabro_Q,  &__q_Cabro_Q);
 }
